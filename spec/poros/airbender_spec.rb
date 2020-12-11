1db51ddae91e31593384b70a7ad266886f249ebb
Sean Steel <seanrsteel@gmail.com>
2020-12-11 10:29:32 -0700

    Add tests for the character poro

---
+++ spec/poros/airbender_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe 'airbender PORO' do
+  it 'creates attributes' do 
+    airbender_data = {:_id=>"5cf5679a915ecad153ab68fd", 
+                      :allies=>["Ozai"], 
+                      :enemies=>["Earth Kingdom"], 
+                      :name=>"Chan (Fire Nation admiral)", 
+                      :affiliation=>"Fire Nation Navy"}
+
+    airbender = Character.new(airbender_data)
+
+    expect(airbender.name).to eq(airbender_data[:name])
+    expect(airbender.image).to eq(airbender_data[:image])
+    expect(airbender.allies).to eq(airbender_data[:allies])
+    expect(airbender.enemies).to eq(airbender_data[:enemies])
+    expect(airbender.affiliation).to eq(airbender_data[:affiliation])
+  end
+end
