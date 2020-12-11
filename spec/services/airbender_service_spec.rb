8945aa42e328d589ad8320a614c252e227c5efb9
Sean Steel <seanrsteel@gmail.com>
2020-12-11 10:36:55 -0700

    Add tests for the airbender service

---
+++ spec/services/airbender_service_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe 'Airbender service' do 
+  it 'will return nation member data' do 
+    response = AirbenderService.nation_members('fire+nation')
+
+    expect(response).to be_an(Array)
+    expect(response[0]).to be_a(Hash)
+
+    expect(response[0]).to have_key(:name)
+    expect(response[0]).to have_key(:allies)
+    expect(response[0]).to have_key(:enemies)
+    expect(response[0]).to have_key(:affiliation)
+
+    expect(response[0][:name]).to be_a(String)
+    expect(response[0][:allies]).to be_a(Array)
+    expect(response[0][:enemies]).to be_a(Array)
+    expect(response[0][:affiliation]).to be_a(String)
+  end
+end
