f1ec05ae863db61edab6cf970db89fbad2e48261
Sean Steel <seanrsteel@gmail.com>
2020-12-11 10:24:10 -0700

    Increase testing for search results page

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,14 +4,5 @@ class SearchController <ApplicationController
     @nation = nation.gsub!('+',' ').titlecase
 
     @characters = SearchFacade.nation_members(nation)
-    # conn = Faraday.new('https://last-airbender-api.herokuapp.com')
-
-    # nation_response = conn.get("api/v1/characters/?affiliation=nation") do |req|
-    #   req.params[:affiliation] = nation
-    # end
-    # nation_response = conn.get("api/v1/characters/?affiliation=nation") 
-    
-    # nation_data = JSON.parse(nation_response.body, symbolize_names: true)
-
   end
 end
--- spec/features/search_for_members_spec.rb
+++ spec/features/search_for_members_spec.rb
@@ -9,27 +9,25 @@ describe 'As a user' do
       expect(current_path).to eq(search_path)
 
       expect(page).to have_content('There are 20 members of the Fire Nation')
-      # Then I should see the total number of people who live in the Fire Nation. (i.e. there are 97 total)
-      # And I should see a list with the detailed information for all 97 members of the Fire Nation.
       within(first('.member')) do
 
         expect(page).to have_css('.name')
-        # breed = find('.name').text
-        # expect(breed).to_not be_empty
+        breed = find('.name').text
+        expect(breed).to_not be_empty
 
         expect(page).to have_css('.image')
         
         expect(page).to have_css('.allies')
-        # breed = find('.allies').text
-        # expect(breed).to_not be_empty
+        breed = find('.allies').text
+        expect(breed).to_not be_empty
 
         expect(page).to have_css('.enemies')
-        # breed = find('.enemies').text
-        # expect(breed).to_not be_empty
+        breed = find('.enemies').text
+        expect(breed).to_not be_empty
 
         expect(page).to have_css('.affiliation')
-        # breed = find('.affiliation').text
-        # expect(breed).to_not be_empty
+        breed = find('.affiliation').text
+        expect(breed).to_not be_empty
       end
     end
   end
